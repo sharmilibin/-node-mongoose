b1ebcd18e14dd30e317074c2bebf73371add7843
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-04-01 23:16:29 -0600

    node mongoose part 1

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/restaurant';
+const connect = mongoose.connect(url);
+connect.then((db) => {
+    console.log('Connected correctly to the server');
+    var newDish = Dishes ({
+        name: 'utahpizza',
+        description: 'unique combination of veggies and creps'
+    });
+    newDish.save()
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({});
+
+    })
+    .then((dishes) =>{
+        console.log(dishes);
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
