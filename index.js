320ecc924f48de88e24cd1eb431af0e20bf79955
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-04-02 19:16:25 -0600

    formatting

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const mongoose = require('mongoose');
+
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/restaurant';
 const connect = mongoose.connect(url);
-connect.then((db) => {
-    console.log('Connected correctly to the server');
-    var newDish = Dishes ({
-        name: 'utahpizza',
-        description: 'unique combination of veggies and creps'
-    });
-    newDish.save()
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({});
 
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
     })
-    .then((dishes) =>{
-        console.log(dishes);
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
+        .then((dish) => {
+            console.log('Printing the inserted dish ::: => ', dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log('Printing the found dishes ::: => ', dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
